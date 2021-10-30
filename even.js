8b2af83b124878ae568c761d32cf7cf20552841d
Jameel <jameel.sameemi@gmail.com>
2021-10-29 20:52:25 -0600

    returning a function

---
+++ even.js
@@ -0,0 +1,12 @@
+const isEven = function (num) {
+  return num % 2 ===0;
+}
+/* const tenIsEven = isEven(10);
+const elevenIsEven = isEven (11);
+console.log (tenIsEven);
+console.log (elevenIsEven);
+ */
+// lets see calling the function without passing 
+// the value to a new variable
+console.log (isEven(10));
+console.log (isEven(11));
