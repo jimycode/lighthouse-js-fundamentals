bc61cfce331a8cc2d94f426d31dfc8f817f11d2b
Jameel <jameel.sameemi@gmail.com>
2021-10-29 20:53:42 -0600

    modified one

--- hello.js
+++ hello.js
@@ -1,6 +1,14 @@
-const sayHello = function (name) {
+/* const sayHello = function (name) {
   console.log ("Hello, " + name);
 }
 sayHello ("jimmy");
 sayHello("jimmycode");
 sayHello("LighthouseLabs");
+ */
+
+const sayHelloToConsole = function (name) {
+  return "Hello, " + name;
+
+}
+const greeting = sayHelloToConsole ('jimmy');
+console.log(greeting);
