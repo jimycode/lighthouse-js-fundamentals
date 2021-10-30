28a5354034c4374fe80c83c47b90af61ebb87b74
Jameel <jameel.sameemi@gmail.com>
2021-10-29 23:16:45 -0600

    age calculator using functions

---
+++ age-calculator.js
@@ -0,0 +1,7 @@
+function ageCalculator (name, yearOfBirth, currentYear) {
+  let age = currentYear-yearOfBirth;
+  let string = name + " is " + age + " years old.";
+  return string;
+ }
+ console.log(ageCalculator("Miranda", 1983, 2015));
+ console.log(ageCalculator("Ferdinand", 1988, 2015));
